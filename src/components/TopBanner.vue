4eea34a39974c8f348e8f0cb9def8ce4a2e93b07
davidalindahl@gmail.com <davidalindahl@gmail.com>
2018-10-02 16:02:19 -0700

    adding v-for bindings to the card component

--- src/components/TopBanner.vue
+++ src/components/TopBanner.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-indigo-darker h-8 w-full flex justify-center">
-    <div class="flex-1">
-      <img class="h-4 text-center" src="../assets/svg/indigo-slate-white.svg" alt="">
+  <div class="bg-indigo-darker h-8 w-full ">
+    <div class="flex justify-center">
+      <img class="h-4 text-center align-middle " src="../assets/svg/indigo-slate-white.svg" alt="">
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import SnackCard from './components/SnackCard';
 
 Vue.config.productionTip = false;
 
@@ -10,4 +11,17 @@ new Vue({
   el: '#app',
   components: { App },
   template: '<App/>',
+});
+
+new Vue({
+  components: { SnackCard },
+  template: '<SnackCard/>',
+
+  el: '#v-for-snacks',
+  data: {
+    snacks: {
+      title: 'Gummy Bears',
+      description: 'There’s just something about our 12 flavors of gummy bears! We promise these are superb quality candies you won’t want to put down.Flavors include cherry, pink grapefruit, watermelon, strawberry, orange, blue raspberry, lime, grape, green apple, mango, pineapple and lemon.',
+    },
+  },
 });
